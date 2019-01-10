21e4e8bd85d342220a44e9480677099190358c5d
Balaji <[23ranganathan.balaji@dxc.com>
2019-01-10 13:47:56 +0530

    changed the file after debugafter changes

--- test.sh
+++ test.sh
@@ -9,16 +9,16 @@ GITHUB1="github.com"
 
 #expected value init for initalization else any other value
 echo "First arg is to determine for the initializing the git repo for the first time or pushing the file : $1"
-echo "Second arg is for the shortid:   $2"
-echo "Third arg is for file name to be pushed : $3"
-echo "Github repo :  $4"
-echo "commit comment:  $4"
+echo "Second arg is for the SHORTID:   $2"
+echo "Third arg is for FILEMAME to be pushed : $3"
+echo "GITHUB Repo :  $4"
+echo "COMMIT the file with comment:  $5"
 
 
 
 if [ "$1" == "init" ] 
 then
-	#pull the folder files from Github.
+	#pull the Repo files from Github.
 	git init
 	git clone git@"${GITHUB1}":"$2"/"$4".git
 	echo "inside if loop :  $2"
@@ -27,6 +27,6 @@ else
 	#push the file 
 	git remote add origin git@"${GITHUB1}":"$2"/"$4".git
 	git add -f "$3"
-	git commit -m "$4"
+	git commit -m "$5"
 	git push origin master
 fi
