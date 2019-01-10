34ccc186c539f3729e41850e9037f579ee336ad6
Balaji <[23ranganathan.balaji@dxc.com>
2019-01-10 12:10:33 +0530

    Initial commit from automation via shell script

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-echo "First arg is to deterine for the initialize of git or pushing the file : $1"
+#variables defined and assigned.
+COLQUESTION="\u001b[36m"
+COLINFO="\u001b[37m"
+COLLOGS="\u001b[35m"
+COLRESET="\u001b[m"
+
+#expected value is init.
+echo "First arg is to determine for the initializing the git repo for the first time or pushing the file : $1"
 echo "Second arg is for the shortid: $2"
 echo "Third arg is for file name:  $3"
 
@@ -9,13 +16,13 @@ if [ "$1" == "init" ]
 then
 	#pull the folder files from Github.
 	git init
-	 git clone git@github.com:rbalaji2/GitDemo.git
-	echo "inside if loop "
+	 git clone git@github.com:"$3"/GitDemo.git
+	echo "inside if loop : ${COLRESET}"
 else
-	echo "inside else loop"
+	echo "inside else loop: ${COLRESET}"
 	#push the file 
 	git remote add origin git@github.com:"$2"/GitDemo.git
-	git add -f $3
+	git add -f "$3"
 	git commit -m "Initial commit from automation via shell script"
 	git push origin master
 fi
